1b94c0abb5a11271cea10a68c6d9616f89772e07
wajeeh__Atrash@outlook.co.il <9e1f0bda8>
2021-07-29 15:38:04 +0300

    basic movments with stream on so we can use ORB_SLAM

---
+++ movementCap.py
@@ -0,0 +1,33 @@
+from djitellopy import tello
+from time import sleep
+#connecting to the drone
+TelloDrone=tello.Tello()
+TelloDrone.connect()
+print("Drone Battery:",TelloDrone.get_battery())
+#turning the drone camera on
+TelloDrone.streamoff()
+TelloDrone.streamon()
+print("run ORB_SLAM2")
+#turn on ORB_SLAM2 then press amy key in the console to let the drone takeoff
+input()
+#drone movment
+TelloDrone.takeoff()
+#(left_righ,for_back,up_down,Yaw)
+TelloDrone.send_rc_control(0,0,10,0)#moving up
+sleep(2)
+TelloDrone.send_rc_control(20,0,0,0)#moving rgiht
+sleep(2)
+TelloDrone.rotate_counter_clockwise(30)#rotating 30 degree counter clock wise
+sleep(2)
+TelloDrone.rotate_clockwise(30)#rotating 30 degree
+sleep(2)
+TelloDrone.send_rc_control(-40,0,0,0)#moving left
+sleep(2)
+TelloDrone.rotate_clockwise(30)#rotating 30 degree
+sleep(2)
+TelloDrone.rotate_counter_clockwise(30)#rotating 30 degree counter clock wise
+sleep(2)
+TelloDrone.send_rc_control(20,0,0,0)
+TelloDrone.land()
+TelloDrone.streamoff()
+print("done")
