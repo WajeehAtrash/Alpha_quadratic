3c9e2450768639bbb2c5630f28f234d5284e98b7
wajeeh__Atrash@outlook.co.il <9e1f0bda8>
2021-08-11 02:04:18 +0300

    updated

--- movementCap.py
+++ movementCap.py
@@ -1,5 +1,12 @@
 from djitellopy import tello
 from time import sleep
+import  cv2
+import os
+import threading
+def run_slam():
+    os.chdir('/home/wajeeh/ORB_SLAM2')
+    OrbSlamCom = './Examples/Monocular/mono_tum Vocabulary/ORBvoc.txt Examples/Monocular/TUM1.yaml'
+    os.system(OrbSlamCom)
 #connecting to the drone
 TelloDrone=tello.Tello()
 TelloDrone.connect()
@@ -9,13 +16,25 @@ TelloDrone.streamoff()
 TelloDrone.streamon()
 print("run ORB_SLAM2")
 #turn on ORB_SLAM2 then press amy key in the console to let the drone takeoff
-input()
+run_Orbslam=threading.Thread(target=run_slam)
+run_Orbslam.start()
 #drone movment
 TelloDrone.takeoff()
-#(left_righ,for_back,up_down,Yaw)
-TelloDrone.send_rc_control(0,0,30,0)#moving up
+TelloDrone.move_up(70)
 for i in range(19):
+    # img=TelloDrone.get_frame_read().frame
+    # img=cv2.resize((img,(640,480)))
+    # cv2.imshow("drone view",img)
+    # cv2.waitKey(1)
     TelloDrone.rotate_clockwise(20)
-    sleep(3)
+    sleep(5)
+TelloDrone.land()
 TelloDrone.streamoff()
 print("done")
+run_Orbslam.join()
+
+
+
+
+
+
